e9d074da846ce6b01d45588a2d190512fc9a96d5
Tommy Werner <wtwerner@gmail.com>
2020-05-09 02:16:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,16 @@
-
-
 def countdown(number)
   while number > 0
-    print "#{number} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
     number -= 1
     sleep 1
   end
-  print "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end
